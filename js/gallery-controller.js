8fbadcd4b212f5bcb59eb575eee7eef41f2d7275
amitfrid7 <fridamit@gmail.com>
2024-02-29 15:24:00 +0200

    Add style

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -4,7 +4,7 @@ function renderGallery() {
     const imgs = gImgs
 
     var srtHtmls = imgs.map(img => `
-    <img src="img/${img.id}.jpg" onclick="onImgSelect(${img.id})">
+    <img src="img/meme-imgs (square)/${img.id}.jpg" onclick="onImgSelect(${img.id})">
     `)
 
     document.querySelector('.gallery').innerHTML = srtHtmls.join('')
--- js/meme-service.js
+++ js/meme-service.js
@@ -6,8 +6,24 @@ const STORAGE_KEY = 'memeDB'
 const gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 
 var gImgs = [
-    { id: 1, url: 'img/1.jpg', keywords: ['funny'] },
-    { id: 2, url: 'img/2.jpg', keywords: ['funny'] },
+    { id: 1, url: 'img/meme-imgs (square)/1.jpg', keywords: ['funny'] },
+    { id: 2, url: 'img/meme-imgs (square)/2.jpg', keywords: ['funny'] },
+    { id: 3, url: 'img/meme-imgs (square)/3.jpg', keywords: ['funny'] },
+    { id: 4, url: 'img/meme-imgs (square)/4.jpg', keywords: ['funny'] },
+    { id: 5, url: 'img/meme-imgs (square)/5.jpg', keywords: ['funny'] },
+    { id: 6, url: 'img/meme-imgs (square)/6.jpg', keywords: ['funny'] },
+    { id: 7, url: 'img/meme-imgs (square)/7.jpg', keywords: ['funny'] },
+    { id: 8, url: 'img/meme-imgs (square)/8.jpg', keywords: ['funny'] },
+    { id: 9, url: 'img/meme-imgs (square)/9.jpg', keywords: ['funny'] },
+    { id: 10, url: 'img/meme-imgs (square)/10.jpg', keywords: ['funny'] },
+    { id: 11, url: 'img/meme-imgs (square)/11.jpg', keywords: ['funny'] },
+    { id: 12, url: 'img/meme-imgs (square)/12.jpg', keywords: ['funny'] },
+    { id: 13, url: 'img/meme-imgs (square)/13.jpg', keywords: ['funny'] },
+    { id: 14, url: 'img/meme-imgs (square)/14.jpg', keywords: ['funny'] },
+    { id: 15, url: 'img/meme-imgs (square)/15.jpg', keywords: ['funny'] },
+    { id: 16, url: 'img/meme-imgs (square)/16.jpg', keywords: ['funny'] },
+    { id: 17, url: 'img/meme-imgs (square)/17.jpg', keywords: ['funny'] },
+    { id: 18, url: 'img/meme-imgs (square)/18.jpg', keywords: ['funny'] },
 ]
 
 var gMeme = {
